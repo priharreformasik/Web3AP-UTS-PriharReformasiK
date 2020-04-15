21b447d72a4583c66d11d3ba98192309f1db5e3c
priharreformasi <refoningrum12@gmail.com>
2020-04-15 15:52:08 +0700

    Updates

---
+++ precache-manifest.015f7230eee799d5980ececdbc93ce53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30871707fcde93e072a723ef77ffecd",
+    "url": "/Web3AP-UTS-PriharReformasiK/index.html"
+  },
+  {
+    "revision": "937643239a966b78ea25",
+    "url": "/Web3AP-UTS-PriharReformasiK/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "9fc4d862fcc7f6043908",
+    "url": "/Web3AP-UTS-PriharReformasiK/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "937643239a966b78ea25",
+    "url": "/Web3AP-UTS-PriharReformasiK/static/js/2.55947f8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Web3AP-UTS-PriharReformasiK/static/js/2.55947f8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc4d862fcc7f6043908",
+    "url": "/Web3AP-UTS-PriharReformasiK/static/js/main.99fd0843.chunk.js"
+  },
+  {
+    "revision": "2783138d7ace950882f4",
+    "url": "/Web3AP-UTS-PriharReformasiK/static/js/runtime-main.14ee28c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Web3AP-UTS-PriharReformasiK/static/media/logo.5d5d9eef.svg"
+  }
+]);
